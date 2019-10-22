3028575c49ffa266ce0358e2b6ff85ca0bca5f59
github username <github email address>
2019-10-22 10:31:33 -0600

    Adds 4 planets

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,32 @@ earth = Planet.new(name: "Earth", distance_from_sun: 92531871, atmosphere: "Brea
     It is a rocky, terrestrial planet, that has a solid and active surface with mountains, valleys, canyons, plains and so much more. 
     Earth is special because it is an ocean planet.")
 
-# new Mez planet
-#Tatiana Test
+jupiter = Planet.new(
+name: “Jupiter”, 
+distance_from_sun: 488293103, 
+atmosphere: “Gassy”,
+description: "Jupiter is the biggest planet in solar system.
+It is covered in swirling cloud stripes. It has big storms like the Great Red Spot, which has been going for hundreds of years. It has rings, but they're too faint to see very well."
+)
 
-#Different test this time
+saturn = Planet.new(
+name: “Saturn”, 
+distance_from_sun: 933368770, 
+atmosphere: “Hot”,
+description: "Saturn is the sixth planet from the Sun. Saturn has the most beautiful rings. They’re made of chunks of ice and rock. Twice every 29 and a half years the great planet Saturn appears ringless."
+)
+
+uranus = Planet.new(
+name: “Uranus”, 
+distance_from_sun: 1751007689, 
+atmosphere: “Gassy”,
+description: "Uranus is the seventh planet from the Sun. It is made of water, methane, and ammonia fluids above a small rocky center. It has 13 faint rings.")
+
+neptune = Planet.new(
+name: “Neptun, 
+distance_from_sun: 2782537545, 
+atmosphere: “Gassy”, 
+description: “Neptun is the eighth planet from the Sun. It is dark, cold, and very windy. It's made of a thick soup of water, ammonia, and methane. Neptune has six rings, but they're very hard to see."
+)
+
 
