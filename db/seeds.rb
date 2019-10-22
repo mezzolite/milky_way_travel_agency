be56b2a3f6093b3bbeed6e8fd00e3605b590d16c
mezzolite <mez.charney@gmail.com>
2019-10-22 09:46:38 -0600

    adds mercury venus mars

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,14 @@ earth = Planet.new(name: "Earth", distance_from_sun: 92531871, atmosphere: "Brea
     It is a rocky, terrestrial planet, that has a solid and active surface with mountains, valleys, canyons, plains and so much more. 
     Earth is special because it is an ocean planet.")
 
+mercury = Planet.new(name: "Mercury", distance_from_sun: 38707465, atmosphere: "Thin, hot, and windy.", 
+    description: "Mercury is the smallest planet in the solar system, and is closest to the Sun. 
+    It is a rocky planet, with a solid, cratered surface.")
 
+venus = Planet.new(name: "Venus", distance_from_sun: 67470137, atmosphere: "Thick, hot, and cloudy.", 
+    description: "Venus is the second planet from the Sun, and the hottest. 
+    Venus is a very active planet, with mountains and volcanoes.")
+
+mars = Planet.new(name: "Mars", distance_from_sun: 153352194, atmosphere: "Thin, cold and rusty.", 
+    description: "Mars is the fourth planet from the Sun. It is a cold desert world that nevertheless has seasons, polar ice caps, volcanoes, canyons, and weather.
+    On some Martian hillsides, there's evidence of liquid salty water in the ground.")
