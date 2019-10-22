6c47bf3035c664c8d24eb917ddf5e2c8b5203589
mezzolite <mez.charney@gmail.com>
2019-10-22 15:22:46 -0600

    creates planet selection logic

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,17 +14,21 @@ end
 def select_planet
     puts "Please enter your preferred destination"
     user_input = gets.chomp
-    selected_planet = display_selected_planet(user_input)
-    puts selected_planet
+    selected_planet = Planet.find_by(name: user_input)
+    if selected_planet
+        display_selected_planet(user_input.capitalize)
+    else  
+        puts "We currently do not offer travel to that destination. Please select from the above list."
+        select_planet
+    end
 end
 
 def display_selected_planet(planet_name)
-    planet = Planet.all.find do |planet|
-        planet.name == planet_name
-    end
+    planet = Planet.where(name: planet_name).limit(1)[0]
         puts "
         Name: #{planet.name}
         Atmosphere: #{planet.atmosphere}
         Description: #{planet.description}"
         system("imgcat ./lib/images/#{planet.image}")
 end
+
